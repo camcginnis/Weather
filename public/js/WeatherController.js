0d4d7e496fac6ec5c2af76fe8d859ecdfff704c6
camcginnis <chad.a.mcginnis@gmail.com>
2015-10-05 20:17:38 -0400

    updated ngWeather2 with more sleek functionality

--- public/js/WeatherController.js
+++ public/js/WeatherController.js
@@ -1,20 +1,21 @@
-app.controller("WeatherController", function($scope, $http){
+app.controller("WeatherController", function($scope, weatherService){
+    $scope.latitude = 0;
+    $scope.longitude = 0;
     $scope.name = "Weather";
     $scope.weatherData = {};
-    var baseUrl = "https://api.forecast.io/forecast/";
-    var baseUrlApi = baseUrl + apiKey + "/";
+    
     
     $scope.getWeather = function(){
-        $http.jsonp(baseUrlApi+$scope.latitude+","+$scope.longitude+"?callback=JSON_CALLBACK")
-        .success(function(response){
-            $scope.weatherData = response;
-                console.log($scope.weatherData);
-            })
-        .error(function(err){
-            console.log(err);
-        });
+       weatherService.get($scope.latitude, $scope.longitude)
+                .success(function(response){
+                    $scope.weatherData = response;
+                    $scope.showWeather = true;
+           
+                    })
        
+                .error(function(err){
+                    $scope.showWeather = false;
+                });
+
     };
-    
-    
 });
--- public/js/app.js
+++ public/js/app.js
@@ -16,3 +16,26 @@ app.config(function($routeProvider){
 
 });
 
+app.factory("weatherService", function($http){
+    var service ={};
+    var baseUrl = "https://api.forecast.io/forecast/";
+    var baseUrlApi = baseUrl + apiKey + "/";
+    
+    //http GET
+    service.get = function(latitude, longitude){
+        
+    return  $http.jsonp(baseUrlApi+latitude+","+longitude+"?callback=JSON_CALLBACK")
+        .success(function(response){
+            console.log(response);
+            return response;
+            })
+        .error(function(err){
+            console.log(err);
+            
+        });
+    };
+    
+    
+    return service;
+    
+});
